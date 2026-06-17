763b9ff118a3b4ac71408617d90cedc73068c6ec
Adit Kurniadi <aditkurniadi00@gmail.com>
2026-06-17 13:14:40 +0700

    [Tahap 3] Implementasi Abstraksi

---
+++ Koneksi/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+abstract class Database {
+    protected $id_tiket;
+    protected $nama_film;
+    protected $jadwal_tayang;
+    protected $jumlah_kursi;
+    protected $harga_dasar_tiket;
+
+    public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $harga_dasar_tiket) {
+        $this->id_tiket = $id_tiket;
+        $this->nama_film = $nama_film;
+        $this->jadwal_tayang = $jadwal_tayang;
+        $this->jumlah_kursi = $jumlah_kursi;
+        $this->harga_dasar_tiket = $harga_dasar_tiket;
+    }
+
+    public abstract function hitungTotalHarga();
+
+    public abstract function tampilkanInfoFasilitas();
+}
